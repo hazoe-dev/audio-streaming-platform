11457e467b68f24dd87530914b5614bef2d53cb7
hazoe-dev <hatran.dev@gmail.com>
2026-01-17 16:36:40 +0700

    chore: bootstrap Spring Boot project (step-2).

---
+++ audiostreaming/src/test/java/dev/hazoe/audiostreaming/AudiostreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hazoe.audiostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiostreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
